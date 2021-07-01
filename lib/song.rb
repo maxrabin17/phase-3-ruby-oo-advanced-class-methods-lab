d4a7455c08ef15ad13683ec7f1aa1ff3cea4adec
maxrabin <max.rabin17@gmail.com>
2021-07-01 11:04:36 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    # if self.find_by_name(name)
+    #   self.find_by_name(name)
+    # else
+    #   self.create_by_name(name)
+    # end
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    # @@all.sort {|a,b| a.name <=> b.name}
+    @@all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    # binding.pry
+    # split_filename = filename.slice(0, filename.length - 4).split(' - ')
+    split_filename = filename.chomp(".mp3").split(' - ')
+    song = self.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    split_filename = filename.chomp(".mp3").split(' - ')
+    song = self.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+    # @@all
+  end
 end
